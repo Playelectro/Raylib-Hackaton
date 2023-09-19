1da86993fcc0b90515e72eb440d0ccf6089aa40d
Playelectro <nebo_GOD@yahoo.com>
2023-09-19 16:38:52 +0300

    Initial Commit with base template

---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+#include <raylib.h>
+
+#include "ball.h"
+
+int main()
+{
+    Color darkGreen = Color{20, 160, 133, 255};
+
+    const int screenWidth = 800;
+    const int screenHeight = 600;
+
+    Ball ball = Ball();
+
+    InitWindow(screenWidth, screenHeight, "Ball game");
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(darkGreen);
+        ball.Update();
+        ball.Draw();
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
+}
