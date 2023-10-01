1273ebabb7fd516453e4e8ce57e02ae62582011d
danbsaucvnush <danbusuioc82@gmail.com>
2023-10-01 21:03:14 +0300

    schimbari

--- Hackaton/learning_state.cpp
+++ Hackaton/learning_state.cpp
@@ -1,6 +1,15 @@
-#include "start_menu_states.h"
+#include "actor.h"
+#include "position_component.h"
+#include "sprite_component.h"
+#include "system_manager.h"
+#include "texture_registry.h"
+#include "learning_state.h"
+#include "sprite_renderer_system.h"
+#include "context_state.h"
+#include "simulation_state.h"
 
-void StartMenuState::InitState() {
+void LearningState::InitState(){
+
 	Actor* actor;
 	actor = new Actor();
 
@@ -17,21 +26,14 @@ void StartMenuState::InitState() {
 	SystemManager::getInstance()->AddActor(actor);
 
 	float buttonWidth = 200, buttonHeight = 100;
-	CreateButton(GetScreenWidth() / 2 - buttonWidth / 2, GetScreenHeight() / 2.5, buttonWidth, buttonHeight, "Simulate","buton", [](Actor* actor) {
+	CreateButton(GetScreenWidth() / 2 - buttonWidth / 2, GetScreenHeight() / 2.5, buttonWidth, buttonHeight, "Simulate", "buton", [](Actor* actor) {
 		ContextState::getInstance()->SetState(new SimulationState());
 		ContextState::getInstance()->InitState();
 		});
 
-	CreateButton(GetScreenWidth() / 2 - buttonWidth / 2, GetScreenHeight() / 1.5, buttonWidth, buttonHeight, "Learn","buton", [](Actor* actor) {
+	CreateButton(GetScreenWidth() / 2 - buttonWidth / 2, GetScreenHeight() / 1.5, buttonWidth, buttonHeight, "Learn", "buton", [](Actor* actor) {
 		ContextState::getInstance()->SetState(new LearningState());
 		ContextState::getInstance()->InitState();
 		});
 
-	SpriteRendererSystem* renderer = new SpriteRendererSystem();
-
-	SystemManager::getInstance()->AddSystem(renderer);
-
-	ButtonMenuSystem* system = new ButtonMenuSystem();
-
-	SystemManager::getInstance()->AddSystem(system);
 }
