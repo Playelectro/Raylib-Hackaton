3e747166a4d32bc0f9073a87c14fd792a880b0b6
oBama68 <alexandrupaval9@yahoo.com>
2023-10-01 14:30:04 +0300

    update physics
    update physics

--- Hackaton/physics_system.cpp
+++ Hackaton/physics_system.cpp
@@ -6,7 +6,7 @@ void PhysicsSystem::doLogic(std::vector<Actor*> actors, int current) {
 
 	PhysicsComponent* actor_pshysics_a = actors[current]->GetComponent<PhysicsComponent>();
 
-	Vector3 rez;
+	Vector3 rez = actor_pshysics_a->velocity;
 
 	for (int i = 0; i < actors.size(); i++) {
 		PositionComponent* actor_position_b = actors[i]->GetComponent<PositionComponent>();
@@ -16,5 +16,13 @@ void PhysicsSystem::doLogic(std::vector<Actor*> actors, int current) {
 		float distance = distanceVectors(actor_position_a->transform.translation, actor_position_b->transform.translation);
 
 		float force = G * actor_pshysics_a->mass * actor_pshysics_b->mass / (distance * distance);
+
+		Vector3 vector = toScalarVector(subtractVectors(actor_pshysics_a->velocity, actor_pshysics_b->velocity), 1 / distance);
+
+		vector = toScalarVector(vector, force);
+
+		rez = addVectors(rez, vector);
 	}
+
+	actor_pshysics_a->velocity = rez;
 }
