b573fb176c77f49c7ef6c86c49d8d76033a7eb6f
oBama68 <alexandrupaval9@yahoo.com>
2023-10-02 19:48:00 +0300

    models
    -added model component and system

---
+++ Hackaton/model_renderer_system.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "system.h"
+#include "model_component.h"
+#include <raylib.h>
+#include "position_component.h"
+
+class ModelRendererSystem : public System {
+public:
+
+	Camera camera;
+
+	ModelRendererSystem() {
+		requirements.push_back(typeid(PositionComponent).name());
+		requirements.push_back(typeid(ModelComponent).name());
+
+		camera = { 0 };
+		camera.position = Vector3{ 0.0f, 0.0f, 0.0f };
+		camera.target = Vector3{ 0.0f, 0.0f, 0.0f };
+		camera.up = Vector3{ 0.0f, 1.0f, 0.0f };
+		camera.fovy = 45.0f;
+		camera.projection = CAMERA_PERSPECTIVE;
+
+	}
+
+	void doLogic(std::vector<Actor*> actors, int current) override;
+
+	void Update() override;
+
+};
